dc51b83967aa40cab8c29d21a8d40b8ac0872151
SocialBureauWeb <web@socialbureau.in>
2026-03-19 14:56:19 +0530

    package.json 2

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 // const connectionURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.zomdyai.mongodb.net/?retryWrites=true&w=majority`;
-const connectionURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_IP}/${process.env.MONGO_DATABASE}?authSource=admin&readPreference=primary&directConnection=true&ssl=false`;
+const connectionURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_IP}/${process.env.MONGO_DATABASE}?authSource=${process.env.MONGO_DATABASE}&readPreference=primary&directConnection=true&ssl=false`;
 
 let sendResponse = {
   appStatusCode: "",
